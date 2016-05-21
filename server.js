99f94a8b83f96db5c4812458b31f53e56476d468
Xuefeng-Zhu <xzhu15@illinois.edu>
2016-05-21 18:17:10 -0500

    use https to enable local test

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-
-var app = express();
-
+var pem = require('pem');
+var https = require('https');
 
 // Since Mixmax calls this API directly from the client-side, it must be whitelisted.
 var corsOptions = {
@@ -11,7 +10,12 @@ var corsOptions = {
 };
 
 
-app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
-app.get('/resolver', cors(corsOptions), require('./api/resolver'));
+pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
+  var app = express();
 
-app.listen(process.env.PORT || 9145);
+  app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
+  app.get('/resolver', cors(corsOptions), require('./api/resolver'));
+
+  https.createServer({key: keys.serviceKey, cert: keys.certificate},
+    app).listen(process.env.PORT || 9145);
+});
